062cac85a4ac14ff427500cc73d70413afbf654c
Minh <congchuabe2000@gmail.com>
2023-04-17 14:03:33 +0700

    BT1

---
+++ studentManagement.sql
@@ -0,0 +1,14 @@
+create database student_management;
+use student_management;
+create table class(
+	id int,
+    name varchar(255)
+);
+create table teacher(
+	id int,
+    name varchar(255),
+    age int,
+    country varchar(255)
+);
+
+
